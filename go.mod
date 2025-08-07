96ca9e6db8ae9a36f07beef84b24ca27dcfaf3e4
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-08-07 22:15:35 +0900

    feat: 初期実装とMakefileの追加

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/aurora1530/image-to-icon-converter
+
+go 1.24.1
+
+require github.com/sergeymakinen/go-ico v1.0.0-beta.0
+
+require github.com/sergeymakinen/go-bmp v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"os"
+	"path/filepath"
+
+	"github.com/sergeymakinen/go-ico"
+)
+
+func main() {
+	// コマンドライン引数の定義
+	inputFile := flag.String("i", "", "入力ファイル名")
+	outputFile := flag.String("o", "", "出力ファイル名")
+	flag.Parse()
+
+	// 入力ファイル名が指定されているか確認
+	if *inputFile == "" {
+		fmt.Fprintln(os.Stderr, "エラー: 入力ファイル名を指定してください。")
+		os.Exit(1)
+	}
+
+	// 入力ファイルの形式を検証
+	ext := filepath.Ext(*inputFile)
+	if ext != ".png" && ext != ".jpg" && ext != ".jpeg" {
+		fmt.Fprintln(os.Stderr, "エラー: サポートされていないファイル形式です。png, jpg, jpegのみがサポートされています。")
+		os.Exit(1)
+	}
+
+	// 画像を読み込む
+	imgFile, err := os.Open(*inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "エラー: 入力ファイルを開けません: %v\n", err)
+		os.Exit(1)
+	}
+	defer imgFile.Close()
+
+	img, _, err := image.Decode(imgFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "エラー: 画像をデコードできません: %v\n", err)
+		os.Exit(1)
+	}
+
+	// 出力ファイル名を決定
+	if *outputFile == "" {
+		baseName := filepath.Base(*inputFile)
+		ext := filepath.Ext(baseName)
+		*outputFile = baseName[:len(baseName)-len(ext)] + ".ico"
+	}
+
+	// 出力ファイルを作成
+	outFile, err := os.Create(*outputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "エラー: 出力ファイルを作成できません: %v\n", err)
+		os.Exit(1)
+	}
+	defer outFile.Close()
+
+	// アイコンに変換して保存
+	err = ico.Encode(outFile, img)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "エラー: アイコン形式にエンコードできません: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("'%s' を '%s' に変換しました。\n", *inputFile, *outputFile)
+}
